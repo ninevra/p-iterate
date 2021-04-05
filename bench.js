100cb76cb1b9cd07742f3d412273a264cad3c273
ninevra <ninevra@gmail.com>
2021-04-04 23:47:59 -0700

    Use benny for benchmarking

--- bench.js
+++ bench.js
@@ -1,4 +1,4 @@
-import Benchmark from 'benchmark';
+import { suite, add, cycle, complete } from 'benny';
 import { pIter, pIterEnumerated, pIterSettled } from 'p-iter';
 import frstcmfrstsvd from 'frstcmfrstsvd';
 
@@ -10,69 +10,34 @@ function* range(start, stop) {
 
 const oneHundredResolved = [...range(0, 100)].map((n) => Promise.resolve(n));
 
-new Benchmark.Suite('process an array of 100 resolved promises')
-  .add('pIter', {
-    defer: true,
-    fn: async (deferred) => {
-      // eslint-disable-next-line no-unused-vars, no-empty
-      for await (const value of pIter(oneHundredResolved)) {
-      }
-
-      deferred.resolve();
-    },
-  })
-  .add('pIterEnumerated', {
-    defer: true,
-    fn: async (deferred) => {
-      // eslint-disable-next-line no-unused-vars, no-empty
-      for await (const value of pIterEnumerated(oneHundredResolved)) {
-      }
-
-      deferred.resolve();
-    },
-  })
-  .add('pIterSettled', {
-    defer: true,
-    fn: async (deferred) => {
-      // eslint-disable-next-line no-unused-vars, no-empty
-      for await (const value of pIterSettled(oneHundredResolved)) {
-      }
-
-      deferred.resolve();
-    },
-  })
-  .add('frstcmfrstsvd', {
-    defer: true,
-    fn: async (deferred) => {
-      // eslint-disable-next-line no-unused-vars, no-empty
-      for await (const result of frstcmfrstsvd(oneHundredResolved)) {
-      }
-
-      deferred.resolve();
-    },
-  })
-  .add('Promise.all', {
-    defer: true,
-    fn: async (deferred) => {
-      await Promise.all(oneHundredResolved);
-
-      deferred.resolve();
-    },
-  })
-  .add('Promise.allSettled', {
-    defer: true,
-    fn: async (deferred) => {
-      await Promise.allSettled(oneHundredResolved);
-
-      deferred.resolve();
-    },
-  })
-  .on('cycle', (event) => {
-    console.log(String(event.target));
-  })
-  .on('complete', function () {
-    console.log(`Fastest is ${this.filter('fastest').map('name')}`);
-  })
-  .run({
-    async: true,
-  });
+suite(
+  'process an array of 100 resolved promises',
+  add('pIter', async () => {
+    // eslint-disable-next-line no-unused-vars, no-empty
+    for await (const value of pIter(oneHundredResolved)) {
+    }
+  }),
+  add('pIterEnumerated', async () => {
+    // eslint-disable-next-line no-unused-vars, no-empty
+    for await (const value of pIterEnumerated(oneHundredResolved)) {
+    }
+  }),
+  add('pIterSettled', async () => {
+    // eslint-disable-next-line no-unused-vars, no-empty
+    for await (const value of pIterSettled(oneHundredResolved)) {
+    }
+  }),
+  add('frstcmfrstsvd', async () => {
+    // eslint-disable-next-line no-unused-vars, no-empty
+    for await (const result of frstcmfrstsvd(oneHundredResolved)) {
+    }
+  }),
+  add('Promise.all', async () => {
+    await Promise.all(oneHundredResolved);
+  }),
+  add('Promise.allSettled', async () => {
+    await Promise.allSettled(oneHundredResolved);
+  }),
+  cycle(),
+  complete()
+);
