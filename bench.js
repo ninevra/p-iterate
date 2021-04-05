d314d2e210444eee57f687c8626a9717b7e8cf12
ninevra <ninevra@gmail.com>
2021-04-05 00:34:56 -0700

    Add more benchmarks

--- bench.js
+++ bench.js
@@ -9,35 +9,79 @@ function* range(start, stop) {
 }
 
 const oneHundredResolved = [...range(0, 100)].map((n) => Promise.resolve(n));
+const oneThousandResolved = [...range(0, 1000)].map((n) => Promise.resolve(n));
+const consumers = [pIter, pIterEnumerated, pIterSettled, frstcmfrstsvd];
 
-suite(
-  'process an array of 100 resolved promises',
-  add('pIter', async () => {
-    // eslint-disable-next-line no-unused-vars, no-empty
-    for await (const value of pIter(oneHundredResolved)) {
-    }
-  }),
-  add('pIterEnumerated', async () => {
-    // eslint-disable-next-line no-unused-vars, no-empty
-    for await (const value of pIterEnumerated(oneHundredResolved)) {
-    }
-  }),
-  add('pIterSettled', async () => {
-    // eslint-disable-next-line no-unused-vars, no-empty
-    for await (const value of pIterSettled(oneHundredResolved)) {
-    }
-  }),
-  add('frstcmfrstsvd', async () => {
-    // eslint-disable-next-line no-unused-vars, no-empty
-    for await (const result of frstcmfrstsvd(oneHundredResolved)) {
-    }
-  }),
-  add('Promise.all', async () => {
-    await Promise.all(oneHundredResolved);
-  }),
-  add('Promise.allSettled', async () => {
-    await Promise.allSettled(oneHundredResolved);
-  }),
-  cycle(),
-  complete()
-);
+(async () => {
+  // Unfortunately, eslint doesn't support top-level await yet
+
+  function consumeAll(consumer, source) {
+    return add(consumer.name, async () => {
+      // eslint-disable-next-line no-unused-vars, no-empty
+      for await (const value of consumer(source)) {
+      }
+    });
+  }
+
+  await suite(
+    'process an array of 100 resolved promises',
+    ...consumers.map((consumer) => consumeAll(consumer, oneHundredResolved)),
+    add('Promise.all', async () => {
+      await Promise.all(oneHundredResolved);
+    }),
+    add('Promise.allSettled', async () => {
+      await Promise.allSettled(oneHundredResolved);
+    }),
+    cycle(),
+    complete()
+  );
+
+  function consumeN(consumer, source, count) {
+    return add(consumer.name, async () => {
+      // eslint-disable-next-line no-unused-vars
+      for await (const value of consumer(oneHundredResolved)) {
+        if (count-- === 0) {
+          break;
+        }
+      }
+    });
+  }
+
+  await suite(
+    'process one value from an array of 100 resolved promises',
+    ...consumers.map((consumer) => consumeN(consumer, oneHundredResolved, 1)),
+    add('Promise.race()', async () => {
+      await Promise.race(oneHundredResolved);
+    }),
+    cycle(),
+    complete()
+  );
+
+  await suite(
+    'process an array of 1000 resolved promises',
+    ...consumers.map((consumer) => consumeAll(consumer, oneThousandResolved)),
+    add('Promise.all', async () => {
+      await Promise.all(oneThousandResolved);
+    }),
+    add('Promise.allSettled', async () => {
+      await Promise.allSettled(oneThousandResolved);
+    }),
+    cycle(),
+    complete()
+  );
+
+  const lastResolved = [
+    ...[...range(0, 99)].map(() => new Promise(() => {})),
+    Promise.resolve(99),
+  ];
+
+  await suite(
+    'process one value from an array of 99 pending and 1 resolved promises',
+    ...consumers.map((consumer) => consumeN(consumer, lastResolved, 1)),
+    add('Promise.race()', async () => {
+      await Promise.race(lastResolved);
+    }),
+    cycle(),
+    complete()
+  );
+})();
